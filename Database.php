3c0fc41fffeb3ab959c8075a70ee654511f25fb3
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2022-03-14 00:10:46 +0300

    Database module

---
+++ Database.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'Config.php';
+
+class DatabaseException extends Exception {}
+
+class Database extends mysqli {
+    function __construct() {
+        parent::__construct(Config::DB_URL, Config::DB_USER, Config::DB_PASS, Config::DB)
+            or throw new DatabaseException("Unable to connect!");
+    }
+    public function getUserByLogin(string $login): array {
+        $f = self::query("SELECT * FROM users WHERE login = '$login'");
+        if ($f->num_rows != 1) throw new DatabaseException("User does not exists!");
+        return $f->fetch_assoc();
+    }
+    public function getUserByKey(string $key): array {
+        $f = self::query("SELECT * FROM users WHERE loginkey = '$key'");
+        if ($f->num_rows != 1) throw new DatabaseException("User does not exists!");
+        return $f->fetch_assoc();
+    }
+}
