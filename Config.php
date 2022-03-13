f18cc52ec3b69d7f511d41d4e57f7b147f2ec55a
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2022-03-14 00:49:22 +0300

    Config module

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+
+class Config {
+    public const DB_URL = '127.0.0.1';
+    public const DB_USER = 'lambda_park';
+    public const DB = 'lambda_park';
+    public const DB_PASS = 'samplepassword';
+}
