96191337b2318400d0ede998e2005d382f601bf6
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2022-03-14 00:10:55 +0300

    Keys generator module

---
+++ Keys.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'Database.php';
+
+class Keys {
+    private static function generate(): string {
+        $db = new Database();
+        do {
+            $g = bin2hex(random_bytes(25));
+            $r = $db->query("SELECT id FROM users WHERE loginkey = '$g'")->num_rows;
+        } while ($r != 0);
+        return $g;
+    }
+    public static function assign(User|int $user): string {
+        $db = new Database();
+        if ($user instanceof User) $user = $user->id;
+        $key = self::generate();
+        $db->query("UPDATE users SET loginkey = '$key' WHERE id = '$user'");
+        return $key;
+    }
+}
