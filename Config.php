b73b59a60327c7a1ce237eddebf21aa5a831714b
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2022-03-16 00:39:24 +0300

    SMTP config

--- Config.php
+++ Config.php
@@ -5,4 +5,9 @@ class Config {
     public const DB_USER = 'lambda_park';
     public const DB = 'lambda_park';
     public const DB_PASS = 'samplepassword';
+
+    public const SMTP_SERV = 'smtp.mail.ru';
+    public const SMTP_USR = 'demo@mail.ru';
+    public const SMTP_USRNAME = 'Demo User';
+    public const SMTP_PWD = '12345'; // "External password" for some resources
 }
