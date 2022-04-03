16dc77e2d764f86e4aadff97c69bdfcd4c726a03
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2022-04-03 21:49:43 +0300

    new exceptions
    `UserAlreadyRegisteredException` & `UserNotFoundException`

--- Exceptions.php
+++ Exceptions.php
@@ -2,3 +2,5 @@
 class DatabaseException extends Exception {}
 class KeyGeneratorException extends Exception {}
 class MailSendFailedException extends Exception {}
+class UserAlreadyRegisteredException extends Exception {}
+class UserNotFoundException extends Exception {}
